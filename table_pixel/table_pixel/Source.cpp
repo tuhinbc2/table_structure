5af559ae88afd958be70f48dba2be2f6c166a5dc
tuhin <tuhinbc2@gmail.com>
2017-03-21 22:46:26 +0600

    table info retrieval ok

--- table_pixel/table_pixel/Source.cpp
+++ table_pixel/table_pixel/Source.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <vector>
 
-enum error_no{
+enum Constants{
+	SUCCESS = 0,
 	INVALID_INDEX = -1,
+
 };
 
 class Dimension
 {
 public:
+	Dimension(){
+
+	}
+
 	Dimension(int length, int number_of_block, int each_border_length)	{
 		reset(length, number_of_block, each_border_length);
 	}
@@ -59,6 +65,10 @@ public:
 		}
 		return start_position_[block_no];
 	}
+
+	int get_size(){
+		return block_count_;
+	}
 private:
 	//in unit
 	std::vector<int> start_position_;
@@ -73,18 +83,74 @@ private:
 class TablePixel
 {
 public:
+	TablePixel(int width, int height, int row, int column, int border_length){
+		reset(width, height, row, column, border_length);
+	}
 
+	void reset(int width, int height, int row, int column, int border_length){
+		x_.reset(width, column, border_length);
+		y_.reset(height, row, border_length);
+	}
+
+	int get_number_of_rows(){
+		return y_.get_size();
+	}
+
+	int get_number_of_columns(){
+		return x_.get_size();
+	}
+
+	int get_start_position(int row_no,int column_no, std::pair<int, int>* position){
+		int x = x_.get_start_position(column_no);
+		int y = y_.get_start_position(row_no);
+
+		if((x == INVALID_INDEX) || (y == INVALID_INDEX)){
+			return INVALID_INDEX;
+		}
+		position->first = x;
+		position->second = y;
+
+		return SUCCESS;
+	}
+
+	int get_end_position(int row_no,int column_no, std::pair<int, int>* position){
+		int x = x_.get_end_position(column_no);
+		int y = y_.get_end_position(row_no);
+
+		if((x == INVALID_INDEX) || (y == INVALID_INDEX)){
+			return INVALID_INDEX;
+		}
+		position->first = x;
+		position->second = y;
+
+		return SUCCESS;
+	}
 private:
+	Dimension x_,y_;
 };
 
 
 int main()
 {
-	Dimension x(100, 3, 10);
+	/*Dimension x(100, 10, 10);
 	std::vector<int> pos;
 	pos = x.get_start_positions();
-	for (int i = 0; i < pos.size();i++){
+	for (int i = 0; i < (int)pos.size();i++){
 		printf("X: %d <---> %d\n", pos[i], x.get_end_position(i));
+	}*/
+
+	TablePixel table(100, 100, 3, 3, 10);
+	std::pair<int, int> val;
+
+	for (int i = 0; i < (int)table.get_number_of_rows();i++){
+		for(int j = 0; j < (int)table.get_number_of_columns();j++){
+			if(table.get_start_position(i, j, &val) == INVALID_INDEX) return (-1);
+			printf("< (%d,%d) X:%2d Y:%2d ----->", i, j, val.first, val.second);
+
+			if(table.get_end_position(i, j, &val) == INVALID_INDEX) return (-1);
+			printf(" X:%2d Y:%2d >\n", val.first, val.second);
+		}
 	}
+
 	return 0;
 }
