ac6e0094fbb0a211eee017a0f5ac79d7a4fc7e3c
tuhin <tuhinbc2@gmail.com>
2017-03-28 23:55:57 +0600

    block > cell. border info. retrieval added.

--- table_pixel/table_pixel/Source.cpp
+++ table_pixel/table_pixel/Source.cpp
@@ -7,6 +7,11 @@ enum Constants{
 
 };
 
+struct ObjDetails
+{
+	int x, y, width, height;
+};
+
 class Dimension
 {
 public:
@@ -14,60 +19,64 @@ public:
 
 	}
 
-	Dimension(int length, int number_of_block, int each_border_length)	{
-		reset(length, number_of_block, each_border_length);
+	Dimension(int length, int number_of_cell, int each_border_length)	{
+		reset(length, number_of_cell, each_border_length);
 	}
 
-	void reset(int total_length, int block, int border_length)	{
+	void reset(int total_length, int cell, int border_length)	{
 		total_length_ = total_length;
-		block_count_ = block;
+		cell_count_ = cell;
 		border_length_ = border_length;
 
 		start_position_.clear();
 		
-		int total_border_len = border_length_ * (block_count_ - 1);
+		int total_border_len = border_length_ * (cell_count_ - 1);
 		int free_space_len = total_length_ - total_border_len;
 
-		int estimated_block_len = free_space_len / block_count_;
-		int extra_length = free_space_len % block_count_;
+		int estimated_cell_len = free_space_len / cell_count_;
+		int extra_length = free_space_len % cell_count_;
 
-		int processed_block_length = 0;
+		int processed_cell_length = 0;
 
-		for (int i = 0; i < block_count_;i++){
-			start_position_.push_back(processed_block_length);
-			int current_block_length = estimated_block_len  + border_length_;
+		for (int i = 0; i < cell_count_;i++){
+			start_position_.push_back(processed_cell_length);
+			int current_cell_length = estimated_cell_len  + border_length_;
 			if (extra_length){
 				extra_length--;
-				current_block_length++;
+				current_cell_length++;
 			}
 
-			processed_block_length += current_block_length;
+			processed_cell_length += current_cell_length;
 		}
 	}
 
-	int get_end_position(int block_no){
-		if (block_no >= block_count_){
+	int get_end_position(int cell_no){
+		if (cell_no >= cell_count_){
 			return INVALID_INDEX;
 		}
-		else if (block_no == block_count_ - 1){
+		else if (cell_no == cell_count_ - 1){
 			return total_length_ - 1;
 		}
-		return start_position_[block_no + 1] - border_length_ - 1;
+		return start_position_[cell_no + 1] - border_length_ - 1;
 	}
 
 	std::vector<int>& get_start_positions(){
 		return start_position_;
 	}
 
-	int get_start_position(int block_no){
-		if (block_no >= block_count_){
+	int get_start_position(int cell_no){
+		if (cell_no >= cell_count_){
 			return INVALID_INDEX;
 		}
-		return start_position_[block_no];
+		return start_position_[cell_no];
 	}
 
 	int get_size(){
-		return block_count_;
+		return cell_count_;
+	}
+
+	int get_border_length(){
+		return border_length_;
 	}
 private:
 	//in unit
@@ -76,7 +85,7 @@ private:
 	int total_length_;
 	//////////////////////
 
-	int block_count_;
+	int cell_count_;
 };
 
 
@@ -112,7 +121,7 @@ public:
 
 		return SUCCESS;
 	}
-
+//ToDo: fix ambiguous order.
 	int get_end_position(int row_no,int column_no, std::pair<int, int>* position){
 		int x = x_.get_end_position(column_no);
 		int y = y_.get_end_position(row_no);
@@ -123,6 +132,36 @@ public:
 		position->first = x;
 		position->second = y;
 
+		return SUCCESS;
+	}
+
+	int get_vertical_border_details(int border_no, ObjDetails *details){
+		int max_border_idx = x_.get_size() - 2;
+		if(border_no > max_border_idx){
+			return INVALID_INDEX;
+		}
+
+		std::pair<int, int> start_pos;
+		get_end_position(y_.get_size() - 1, border_no, &start_pos);
+		details->x = start_pos.first + 1;
+		details->y = 0;
+		details->width = y_.get_border_length();
+		details->height = start_pos.second + 1 ;
+		return SUCCESS;
+	}
+
+	int get_horizontal_border_details(int border_no, ObjDetails *details){
+		int max_border_idx = y_.get_size() - 2;
+		if(border_no > max_border_idx){
+			return INVALID_INDEX;
+		}
+
+		std::pair<int, int> start_pos;
+		get_end_position(border_no, x_.get_size() - 1, &start_pos);
+		details->y = start_pos.second + 1;
+		details->x = 0;
+		details->height = x_.get_border_length();
+		details->width = start_pos.first + 1 ;
 		return SUCCESS;
 	}
 private:
@@ -149,6 +188,18 @@ int main()
 
 			if(table.get_end_position(i, j, &val) == INVALID_INDEX) return (-1);
 			printf(" X:%2d Y:%2d >\n", val.first, val.second);
+		}
+
+		ObjDetails details;
+		if(table.get_vertical_border_details(i, &details) == SUCCESS){
+			printf("|| no: %d x:%d y:%d height:%d width:%d\n", i, details.x, details.y, details.height, details.width);
+			if(table.get_horizontal_border_details(i, &details) == SUCCESS){
+				printf("-- no: %d x:%d y:%d height:%d width:%d\n", i, details.x, details.y, details.height, details.width);
+			}else{
+				printf("-- no. invalid\n");
+			}
+		}else{
+			printf("|| no. invalid\n");
 		}
 	}
 
